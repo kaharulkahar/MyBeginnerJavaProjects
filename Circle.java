83c9fdd39fd55ddebb56cec89cc019e676086661
kaharulkahar <kaharulkahar@gmail.com>
2024-06-11 19:31:00 +0900

    Java program to calculate Circle

---
+++ Circle.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import javax.swing.*;
+
+public class Circle {
+	//attributes
+	private int radius;
+	final double PI = 3.14;
+	
+
+	//method	
+	public void setRadius(int r) {
+		radius = r;
+	}
+	
+	public int getRadius() {
+		return radius;
+	}
+	
+	public double computeArea( ) {
+		return PI* Math.pow(radius, 2);
+	}
+	
+	//main 
+	public static void main (String [] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int y, z, area;
+		String y1;
+		
+		y1 = JOptionPane.showInputDialog ("Please set radius.");
+		y = Integer.parseInt (y1);
+		
+		Circle c1 = new Circle ();	
+		c1.setRadius(y);
+		z = c1.getRadius();
+		area = (int) c1.computeArea();
+		
+		System.out.println("The radius is "+ z);
+		JOptionPane.showMessageDialog (null,"The radius is "+ z);
+		
+		System.out.println("The area of the circle is "+ area);
+		JOptionPane.showMessageDialog (null,"The area of the circle is "+ area);
+	}
+}
